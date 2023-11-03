357b30aefb6610bf929c5686a47c40e7d22735e5
Matias Zanan <matiaszanan@gmail.com>
2023-11-02 21:53:17 -0300

    [Data Acquisition] Create mobile screen

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,11 +6,8 @@ module.exports = {
       colors: {
         primary: "#F24E70",
         secondary: "#D6D6FF",
-        white: "#F2F2F2",
-        background: "#F2F2F2",
-
         surface: "#010101",
-        text: "black",
+        "light-gray": "#F2F2F2",
       },
     },
   },
